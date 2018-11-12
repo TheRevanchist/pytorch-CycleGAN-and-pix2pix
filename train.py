5484146b0c7de81b62b4db3963d633d7d672ceff
root <ismail.elezi@gmail.com>
2018-11-12 14:27:28 +0000

    Add train

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import time
 from options.train_options import TrainOptions
 from data import CreateDataLoader
 from models import create_model
-from util.visualizer import Visualizer
+# from util.visualizer import Visualizer
 
 if __name__ == '__main__':
     opt = TrainOptions().parse()
@@ -13,7 +13,7 @@ if __name__ == '__main__':
 
     model = create_model(opt)
     model.setup(opt)
-    visualizer = Visualizer(opt)
+    # visualizer = Visualizer(opt)
     total_steps = 0
 
     for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
@@ -25,7 +25,7 @@ if __name__ == '__main__':
             iter_start_time = time.time()
             if total_steps % opt.print_freq == 0:
                 t_data = iter_start_time - iter_data_time
-            visualizer.reset()
+            # visualizer.reset()
             total_steps += opt.batch_size
             epoch_iter += opt.batch_size
             model.set_input(data)
@@ -33,14 +33,15 @@ if __name__ == '__main__':
 
             if total_steps % opt.display_freq == 0:
                 save_result = total_steps % opt.update_html_freq == 0
-                visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
+                # visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
 
             if total_steps % opt.print_freq == 0:
                 losses = model.get_current_losses()
                 t = (time.time() - iter_start_time) / opt.batch_size
-                visualizer.print_current_losses(epoch, epoch_iter, losses, t, t_data)
-                if opt.display_id > 0:
-                    visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, opt, losses)
+                print(losses)
+                # visualizer.print_current_losses(epoch, epoch_iter, losses, t, t_data)
+                # if opt.display_id > 0:
+                    # visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, opt, losses)
 
             if total_steps % opt.save_latest_freq == 0:
                 print('saving the latest model (epoch %d, total_steps %d)' %
