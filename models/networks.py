4b49083360db879a260d5980267ecb4c18ec37d9
root <ismail.elezi@gmail.com>
2018-11-15 15:09:31 +0000

    Remove the written scheduler in favor of PyTorch one

--- models/networks.py
+++ models/networks.py
@@ -26,7 +26,8 @@ def get_scheduler(optimizer, opt):
         def lambda_rule(epoch):
             lr_l = 1.0 - max(0, epoch + opt.epoch_count - opt.niter) / float(opt.niter_decay + 1)
             return lr_l
-        scheduler = LambdaLR(optimizer, lr_lambda=lambda_rule)
+        #scheduler = LambdaLR(optimizer, lr_lambda=lambda_rule)
+        scheduler = lr_scheduler.LambdaLR(optimizer.optimizer, lr_lambda=lambda_rule)
     elif opt.lr_policy == 'step':
         scheduler = lr_scheduler.StepLR(optimizer, step_size=opt.lr_decay_iters, gamma=0.1)
     elif opt.lr_policy == 'plateau':
